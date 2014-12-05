d3cf842dece8f26dd4891caf69870498ab778793
Furushchev <furushchev@mail.ru>
2014-12-05 12:57:33 +0900

    pub pose

--- src/psmove_tracker.cpp
+++ src/psmove_tracker.cpp
@@ -1,10 +1,17 @@
+//#include <cmath>
 #include <vector>
+#include <string>
+
+#include <boost/function.hpp>
 
 #include <ros/ros.h>
-#include <geometry_msgs/Vector3Stamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <jsk_2014w_rinko/PSMoveButton.h>
+
 #include <psmove.h>
 
 #include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -16,8 +23,11 @@
 #define PSEYE_FOV_BLUE_DOT 75
 #define PSEYE_FOV_RED_DOT 56
 
+#define NIL (-1)
+#define DEBUG
 
-//#define DEBUG
+float qx = 0.f, qy = 0.f, qz = 0.f, qw = 0.f;
+bool has_orientation = false;
 
 struct buffered_vector {
 public:
@@ -28,9 +38,7 @@ public:
     n_ = n;
     data = std::vector<float>();
     data.resize(n);
-    for (int i = 0; i < n_; ++i) data[i] = 0.f;
-    p = 0;
-    length = 0;
+    this->clear();
   };
   void insert(float v){
     data[p] = v;
@@ -39,14 +47,19 @@ public:
     if (length < 3) length++;
   }
   float average(){
-    if (length <= 0) return 0;
+    if (length <= 0) return 0.f;
     float sum = 0;
     for (int i = 0; i < length; i++) sum += data[i];
     return sum / length;
   }
   float latest(){
     if(length != 0) return data[p];
-    else return 0;
+    else return 0.f;
+  }
+  void clear(){
+    for (int i = 0; i < n_; ++i) data[i] = 0.f;
+    length = 0;
+    p = 0;
   }
 };
 
@@ -57,8 +70,10 @@ public:
   glm::vec4 viewport;
 
   fusion(float width, float height, float z_near, float z_far): width(width), height(height){
+    ROS_INFO_STREAM("fusion initialized");
     projection = glm::perspectiveFov<float>(PSEYE_FOV_BLUE_DOT, width, height, z_near, z_far);
     viewport = glm::vec4(0., 0., width, height);
+    std::cout << "HEY" << std::endl;
   };
 
   cv::Vec3f get_position(float camX, float camY, float camR){
@@ -72,30 +87,30 @@ public:
     float step = .25;
 
     while (step > PSMOVE_FUSION_STEP_EPSILON) {
-        /* Calculate center position of sphere */
-        obj = glm::unProject(glm::vec3(winX, winY, winZ),
-                glm::mat4(), projection, viewport);
+      /* Calculate center position of sphere */
+      obj = glm::unProject(glm::vec3(winX, winY, winZ),
+                           glm::mat4(), projection, viewport);
 
-        /* Project left edge center of sphere */
-        glm::vec3 left = glm::project(glm::vec3(obj.x - .5, obj.y, obj.z),
-                glm::mat4(), projection, viewport);
+      /* Project left edge center of sphere */
+      glm::vec3 left = glm::project(glm::vec3(obj.x - .5, obj.y, obj.z),
+                                    glm::mat4(), projection, viewport);
 
-        /* Project right edge center of sphere */
-        glm::vec3 right = glm::project(glm::vec3(obj.x + .5, obj.y, obj.z),
-                glm::mat4(), projection, viewport);
+      /* Project right edge center of sphere */
+      glm::vec3 right = glm::project(glm::vec3(obj.x + .5, obj.y, obj.z),
+                                     glm::mat4(), projection, viewport);
 
-        float width_ = (right.x - left.x);
-        if (width_ > targetWidth) {
-            /* Too near */
-            winZ += step;
-        } else if (width_ < targetWidth) {
-            /* Too far away */
-            winZ -= step;
-        } else {
-            /* Perfect fit */
-            break;
-        }
-        step *= .5;
+      float width_ = (right.x - left.x);
+      if (width_ > targetWidth) {
+        /* Too near */
+        winZ += step;
+      } else if (width_ < targetWidth) {
+        /* Too far away */
+        winZ -= step;
+      } else {
+        /* Perfect fit */
+        break;
+      }
+      step *= .5;
     }
     return cv::Vec3f(obj.x, obj.y, obj.z);
   }
@@ -104,107 +119,107 @@ public:
 
 void min_max_color(int color, int threshold, int& min_color, int& max_color)
 {
-  min_color = (color - threshold < 0) ? 0 : color - threshold;
-  max_color = (color + threshold > 255) ? 255 : color + threshold;
+  if (color == NIL){
+    min_color = 0;
+    max_color = 255;
+  } else {
+    min_color = (color - threshold < 0) ? 0 : color - threshold;
+    max_color = (color + threshold > 255) ? 255 : color + threshold;
+  }
 }
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "psmove_tracker");
+class PSMoveTracker {
+  ros::NodeHandle nh_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
 
+  int color_r_min_, color_r_max_;
+  int color_g_min_, color_g_max_;
+  int color_b_min_, color_b_max_;
+
+  int color_threshold_;
+  int radius_smoothing_threshold_;
+  int roi_margin_2_;
+
+  cv::Point2f prev_centroid_;
+  buffered_vector* prev_radius_;
+  buffered_vector* prev_centroid_x_;
+  buffered_vector* prev_centroid_y_;
+  bool force_not_roi_;
+
+  fusion* fusion_;
+  bool fusion_alloced;
+  double z_near_, z_far_;
+
+public:
   ros::NodeHandle nh;
-  int camera_id = 0;
-  nh.getParam("camera_id", camera_id);
-  int color_r = 255, color_g = 0, color_b = 255;
-  nh.getParam("color_r", color_r);
-  nh.getParam("color_g", color_g);
-  nh.getParam("color_b", color_b);
+  int color_r, color_g, color_b;
 
-  int color_threshold = 20; // 20;
-  nh.getParam("color_threshold" , color_threshold);
+  PSMoveTracker(): nh(), nh_("~"), prev_centroid_(0.f, 0.f), force_not_roi_(false){
+    nh_.param<int>("color_r", color_r, NIL);
+    nh_.param<int>("color_g", color_g, NIL);
+    nh_.param<int>("color_b", color_b, NIL);
+    nh_.param<int>("color_threshold" , color_threshold_, 20);
 
-  ros::Publisher pub = nh.advertise<geometry_msgs::Vector3Stamped>("/psmove_circle", 1000);
+    ROS_INFO_STREAM("R: " << color_r << " G: " << color_g << " B: " << color_b << " TH: " << color_threshold_);
 
-  int color_r_min, color_r_max;
-  int color_g_min, color_g_max;
-  int color_b_min, color_b_max;
-  min_max_color(color_r, color_threshold, color_r_min, color_r_max);
-  min_max_color(color_g, color_threshold, color_g_min, color_g_max);
-  min_max_color(color_b, color_threshold, color_b_min, color_b_max);
+    min_max_color(color_r, color_threshold_, color_r_min_, color_r_max_);
+    min_max_color(color_g, color_threshold_, color_g_min_, color_g_max_);
+    min_max_color(color_b, color_threshold_, color_b_min_, color_b_max_);
 
-  cv::VideoCapture cap(camera_id);
-  if(!cap.isOpened()){
-    ROS_FATAL_STREAM("failed to open camera");
-    return -1;
-  }
+    nh_.param<int>("radius_smoothing_threshold", radius_smoothing_threshold_, 5);
+    nh_.param<int>("roi_margin_2", roi_margin_2_, 50);
 
-  //load calib data
-  std::string intrinsics_xml = "/home/leus/.psmoveapi/intrinsics.xml";
-  nh.getParam("intrinsics_xml", intrinsics_xml);
-  cv::FileStorage intr_fs(intrinsics_xml, cv::FileStorage::READ);
-  cv::Mat camera_matrix;
-  if(!intr_fs.isOpened()) ROS_WARN_STREAM("failed to open intrinsics xml at: " << intrinsics_xml);
-  intr_fs["Camera_matrix"] >> camera_matrix;
-  intr_fs.release();
+    prev_radius_ = new buffered_vector(radius_smoothing_threshold_);
+    prev_centroid_x_ = new buffered_vector(radius_smoothing_threshold_);
+    prev_centroid_y_ = new buffered_vector(radius_smoothing_threshold_);
 
-  std::string distortion_xml = "/home/leus/.psmoveapi/distortion.xml";
-  nh.getParam("distortion_xml", distortion_xml);
-  cv::FileStorage dist_fs(distortion_xml, cv::FileStorage::READ);
-  cv::Mat dist_coeff;
-  if(!dist_fs.isOpened()) ROS_WARN_STREAM("failed to open distortion xml at: " << distortion_xml);
-  dist_fs["Distortion"] >> dist_coeff;
-  dist_fs.release();
-  
-  cv::Size captureSize(cap.get(CV_CAP_PROP_FRAME_WIDTH), cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+    nh_.param<double>("z_near", z_near_, 1.);
+    nh_.param<double>("z_far", z_far_, 1000.);
 
-  cv::Mat new_camera_matrix = cv::getOptimalNewCameraMatrix(camera_matrix, dist_coeff, captureSize, 1, captureSize, 0);
+    fusion_alloced = false;
 
-  ros::Rate loop_rate(1000);
+    pub_ = nh.advertise<geometry_msgs::PoseStamped>("psmove", 1000);
+    sub_ = nh.subscribe("image", 1, &PSMoveTracker::imageCallback, this);
+  };
 
-  geometry_msgs::Vector3Stamped msg;
+  ~PSMoveTracker(){
+    delete prev_radius_;
+    delete prev_centroid_x_;
+    delete prev_centroid_y_;
+    delete fusion_;
+  };
 
-  psmove_init(PSMOVE_CURRENT_VERSION);
-  PSMove *move = psmove_connect();
-  
-  cv::Point2f prev_centroid(0.f,0.f);
-  buffered_vector prev_radius(3);
-  float roi_margin_2 = 50;
-  bool force_not_roi = false;
-
-  fusion fusion(captureSize.width, captureSize.height, 1, 1000);
-
-  while (ros::ok())
-  {
-    int res = psmove_poll(move);
-    if (res)
-    {
-      psmove_set_leds(move, 255, 0, 255);
-      psmove_update_leds(move);
+  void imageCallback(const sensor_msgs::ImageConstPtr& msg){
+    if(!fusion_alloced){
+      fusion_ = new fusion(msg->width, msg->height, z_near_, z_far_);
+      fusion_alloced = true;
     }
 
+    cv_bridge::CvImageConstPtr cvimg = cv_bridge::toCvShare(msg);
 
-    cv::Mat raw_frame, frame, filtered, roi_image;
-    cap >> raw_frame;
+    cv::Mat frame = cvimg->image;
+    cv::Mat filtered, roi_image;
 
-    cv::undistort(raw_frame, frame, camera_matrix, dist_coeff, new_camera_matrix);
-
-    // roi
     bool is_roi = false;
     cv::Rect roi_rect;
-    if (prev_radius.latest() <= 0 || force_not_roi){
+    if (prev_radius_->latest() <= 0 || force_not_roi_){
       roi_image = frame;
-      force_not_roi = false;
+      force_not_roi_ = false;
     }
     else {
+      // set roi
       is_roi = true;
-      roi_rect = cv::Rect(prev_centroid.x - prev_radius.latest() - roi_margin_2,
-                        prev_centroid.y - prev_radius.latest() - roi_margin_2,
-                        (prev_radius.latest() + roi_margin_2) * 2,
-                        (prev_radius.latest() + roi_margin_2) * 2);
+      int roi_rect_window_2 = prev_radius_->latest() + roi_margin_2_;
+      roi_rect = cv::Rect(prev_centroid_.x - roi_rect_window_2,
+                          prev_centroid_.y - roi_rect_window_2,
+                          cv::min(roi_rect_window_2 * 2, frame.size().width),
+                          cv::min(roi_rect_window_2 * 2, frame.size().height));
       if (roi_rect.x < 0) roi_rect.x = 0;
       if (roi_rect.y < 0) roi_rect.y = 0;
       if (roi_rect.x + roi_rect.width > frame.size().width) roi_rect.x = frame.size().width - roi_rect.width;
       if (roi_rect.y + roi_rect.height > frame.size().height) roi_rect.y = frame.size().height - roi_rect.height;
+
       roi_image = frame(roi_rect);
 
 #ifdef DEBUG
@@ -213,7 +228,7 @@ int main(int argc, char** argv)
     }
 
     // color filter
-    cv::inRange(roi_image, cv::Scalar(color_b_min, 0, color_r_min), cv::Scalar(color_b_max, 255, color_r_max), filtered);
+    cv::inRange(roi_image, cv::Scalar(color_b_min_, color_g_min_, color_r_min_), cv::Scalar(color_b_max_, color_g_max_, color_r_max_), filtered);
     cv::GaussianBlur(filtered, filtered, cv::Size(9,9), 2, 2);
 
     // circle detection
@@ -222,11 +237,11 @@ int main(int argc, char** argv)
 
     float cent_x, cent_y;
     if (circles.size() <= 0) {
-#ifdef DEBUG
+//#ifdef DEBUG
       ROS_INFO_STREAM("no circle detected.");
-#endif
-      force_not_roi = true;
-      continue;
+//#endif
+        force_not_roi_ = true;
+        return;
     }
 
     float max_radius = 0.0f;
@@ -238,36 +253,134 @@ int main(int argc, char** argv)
         cent_y = circles[i][1];
       }
     }
-    prev_radius.insert(max_radius);
     if (is_roi) {
       cent_x += roi_rect.x;
       cent_y += roi_rect.y;
     }
-    prev_centroid.x = cent_x;
-    prev_centroid.y = cent_y;
-    float avg_radius = prev_radius.average();
+    prev_centroid_.x = cent_x;
+    prev_centroid_.y = cent_y;
+
+    prev_radius_->insert(max_radius);
+    prev_centroid_x_->insert(cent_x);
+    prev_centroid_y_->insert(cent_y);
+
+    float avg_radius = prev_radius_->average();
+    float avg_cent_x = prev_centroid_x_->average();
+    float avg_cent_y = prev_centroid_y_->average();
 #ifdef DEBUG
-    ROS_INFO_STREAM("x: " << cent_x << " y: " << cent_y << " r: " << avg_radius);
-#endif
-    cv::circle( frame, cv::Point(cent_x, cent_y), avg_radius, cv::Scalar(0, 0, 255), 3, 8, 0);
-#ifdef DEBUG
-   cv::imshow("debug1", filtered);
-   cv::imshow("debug2", frame);
+    ROS_INFO_STREAM("x: " << avg_cent_x << " y: " << avg_cent_y << " r: " << avg_radius);
+    cv::Mat frame_copy = frame.clone();
+    cv::circle( frame_copy, cv::Point(avg_cent_x, avg_cent_y), avg_radius, cv::Scalar(0, 0, 255), 3, 8, 0);
+    cv::imshow("debug1", filtered);
+    cv::imshow("debug2", frame_copy);
     cvWaitKey(1);
 #endif
 
-    cv::Point3f pos = fusion.get_position(cent_x, cent_y, avg_radius);
+    cv::Point3f pos = fusion_->get_position(avg_cent_x, avg_cent_y, avg_radius);
 #ifdef DEBUG
     ROS_INFO_STREAM("pos x: " << pos.x << " y: " << pos.y << " z: " << pos.z);
 #endif
 
-    msg.vector.x = cent_x;
-    msg.vector.y = cent_y;
-    msg.vector.z = avg_radius;
-    pub.publish(msg);
+    // publish pose
+    geometry_msgs::PoseStamped pose;
+    pose.header.frame_id = "map";
+    pose.pose.position.x = pos.x;
+    pose.pose.position.y = pos.y;
+    pose.pose.position.z = pos.z;
+    if(has_orientation){
+      pose.pose.orientation.x = qx;
+      pose.pose.orientation.y = qy;
+      pose.pose.orientation.z = qz;
+      pose.pose.orientation.w = qw;
+    }
+    pub_.publish(pose);
+  }
+};
 
-    loop_rate.sleep();
+std::vector<unsigned char> get_button_chars(PSMove* move){
+    unsigned int b = psmove_get_buttons(move);
+    std::vector<unsigned char> ret;
+    if(b & Btn_PS) ret.push_back('p');
+    if(b & Btn_TRIANGLE) ret.push_back('t');
+    if(b & Btn_CIRCLE) ret.push_back('c');
+    if(b & Btn_CROSS) ret.push_back('x');
+    if(b & Btn_SQUARE) ret.push_back('s');
+    if(b & Btn_SELECT) ret.push_back('0');
+    if(b & Btn_START) ret.push_back('1');
+    if(b & Btn_MOVE) ret.push_back('m');
+    if(b & Btn_T) ret.push_back('l');
+    return ret;
   }
 
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "psmove_tracker");
+
+  PSMoveTracker p;
+
+  PSMove* move_;
+  psmove_init(PSMOVE_CURRENT_VERSION);
+  move_ = psmove_connect();
+
+  if (!move_){
+    ROS_FATAL_STREAM("failed to connect to default move controller.\n" << "Please connect via USB or BT.");
+    exit(1);
+  }
+
+  char *serial = psmove_get_serial(move_);
+  ROS_INFO_STREAM("serial: " << serial);
+  free(serial);
+
+  enum PSMove_Connection_Type ctype = psmove_connection_type(move_);
+  switch(ctype)
+  {
+  case Conn_USB:
+    ROS_INFO_STREAM("connected via usb");
+    break;
+  case Conn_Bluetooth:
+    ROS_INFO_STREAM("connected via bluetooth");
+    break;
+  default:
+    ROS_INFO_STREAM("unknown connection type");
+    break;
+  }
+  psmove_enable_orientation(move_, PSMove_True);
+
+  ros::Publisher btn_pub = p.nh.advertise<jsk_2014w_rinko::PSMoveButton>("psmove_button", 1000);
+
+  ros::Rate loop_rate_(30);
+
+  while(ros::ok()){
+    // get psmove sensor information
+    int res = psmove_poll(move_);
+    if (res){
+      psmove_set_leds(move_, p.color_r, 0, p.color_b);
+      psmove_update_leds(move_);
+      if (psmove_has_orientation(move_) == PSMove_True)
+        has_orientation = true;
+      psmove_get_orientation(move_, &qx, &qy, &qz, &qw);
+
+      try {
+        std::vector<unsigned char> btn = get_button_chars(move_);
+        unsigned char trigger_val = psmove_get_trigger(move_);
+        jsk_2014w_rinko::PSMoveButton btn_msg;
+        btn_msg.button = btn;
+        btn_msg.trigger = trigger_val / 255.0f;
+        btn_pub.publish(btn_msg);
+  } catch (...) {
+    ROS_WARN_STREAM("failed to publish button");
+  }
+
+    } else {
+//      ROS_WARN_STREAM("failed ay psmove_poll");
+    }
+
+    ros::spinOnce();
+    loop_rate_.sleep();
+  }
+
+  psmove_disconnect(move_);
   return 0;
 }
