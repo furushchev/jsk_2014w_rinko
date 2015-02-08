46edc6ee821810d6439a628a9e2685e505725f35
Furushchev <furushchev@pr1012.(none)>
2015-02-08 03:29:23 -0800

    Float32MultiArray -> PointStamped

--- scripts/gaze_visualizer_rviz.py
+++ scripts/gaze_visualizer_rviz.py
@@ -3,41 +3,42 @@
 
 import rospy
 
-from std_msgs.msg import Float32MultiArray, ColorRGBA
+from std_msgs.msg import ColorRGBA
+from geometry_msgs.msg import PointStamped
 from jsk_rviz_plugins.msg import OverlayText
 
 prev_msg = None
 pub = None
 r = None
 
+send_msg = OverlayText()
+send_msg.text = "*"
+send_msg.width = 140
+send_msg.height = 200
+send_msg.text_size = 100
+send_msg.line_width = 2
+send_msg.font = "DejaVu Sans Mono"
+send_msg.bg_color = ColorRGBA(0,0,0,0)
+send_msg.fg_color = ColorRGBA(255, 0, 0, 1)
+
 
 def gaze_callback(msg):
-    global prev_msg
+    # global prev_msg
     x = msg.data[0]
     y = msg.data[1]
-    rospy.loginfo("x: " + str(x) + ", y: " + str(y))
-    send_msg = OverlayText()
-    send_msg.text = "*"
-    send_msg.width = 140
-    send_msg.height = 200
-    send_msg.text_size = 100
-    send_msg.line_width = 2
-    send_msg.font = "DejaVu Sans Mono"
-    send_msg.bg_color = ColorRGBA(0,0,0,0)
-    send_msg.fg_color = ColorRGBA(255, 0, 0, 1)
     send_msg.left = x
     send_msg.top = y
     pub.publish(send_msg)
-    if prev_msg:
-        prev_msg.action = 1
-#        pub.publish(prev_msg)
-    prev_msg = send_msg
-    r.sleep()
+    # if prev_msg:
+    #     prev_msg.action = 1
+    #     pub.publish(send_msg)
+    # prev_msg = send_msg
+    # r.sleep()
 
 if __name__ == '__main__':
     global pub, r
     rospy.init_node('gaze_visualizer_rviz')
     pub = rospy.Publisher('/gaze_overlay', OverlayText)
-    sub = rospy.Subscriber('/gaze', Float32MultiArray, gaze_callback, tcp_nodelay=True)
+    sub = rospy.Subscriber('/gaze', PointStamped, gaze_callback)
     r = rospy.Rate(30)
     rospy.spin()
